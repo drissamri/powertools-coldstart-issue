fd09eb50137e1116a30c47c218b3af1a403cc546
Driss Amri <driss.amri@gmail.com>
2020-10-06 00:09:35 +0200

    no message

--- README.md
+++ README.md
@@ -15,15 +15,29 @@ lambda-logging (baseline)
 {
   "functionName": "client-service-dev-create-client",
   "memorySize": 3008,
-  "coldStarts": 65,
-  "min": 1475.29,
-  "p25": 1573.41,
-  "median": 1602.8,
-  "p75": 1624.75,
-  "p95": 1669.79,
-  "max": 1712.77,
-  "stddev": 43.1292
+  "coldStarts": 47,
+  "min": 1266.24,
+  "p25": 1321.98,
+  "median": 1353.7,
+  "p75": 1368.37,
+  "p95": 1397.98,
+  "max": 1410.66,
+  "stddev": 32.9884
 }
+
+{
+  "functionName": "client-service-dev-create-client",
+  "memorySize": 3008,
+  "coldStarts": 65,
+  "min": 1281.9,
+  "p25": 1333.04,
+  "median": 1350.91,
+  "p75": 1365.49,
+  "p95": 1435.17,
+  "max": 1706.47,
+  "stddev": 55.5152
+}
+
 ```
 powertools-logging
 ```
--- src/main/java/com/drissamri/client/core/AppConfig.java
+++ src/main/java/com/drissamri/client/core/AppConfig.java
@@ -1,9 +1,7 @@
 package com.drissamri.client.core;
 
-import com.amazonaws.xray.interceptors.TracingInterceptor;
 import com.drissamri.client.service.ClientService;
 import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
-import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
 import software.amazon.awssdk.http.crt.AwsCrtAsyncHttpClient;
 import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient;
 
@@ -12,9 +10,6 @@ public class AppConfig {
         return DynamoDbAsyncClient.builder()
                 .httpClientBuilder(AwsCrtAsyncHttpClient.builder().maxConcurrency(50))
                 .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
-                .overrideConfiguration(ClientOverrideConfiguration.builder()
-                        .addExecutionInterceptor(new TracingInterceptor())
-                        .build())
                 .build();
     }
 
